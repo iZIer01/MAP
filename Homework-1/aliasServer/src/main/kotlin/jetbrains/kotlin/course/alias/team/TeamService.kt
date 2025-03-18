1348316628dd21d95dd2a71523f4c582cf74953c
iZIer01 <joaonkunku01@gmail.com>
2025-03-18 22:57:16 +0200

    Card section and teams seaction done

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,25 @@
 package jetbrains.kotlin.course.alias.team
 
 import org.springframework.stereotype.Service
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 
 @Service
 class TeamService {
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
-}
+    //  Define the IdentifierFactory to help generate unique IDs
+
+    // Creating a map to store the unique id of teams
+    companion object {
+        val teamsStorage: TeamMap<Identifier,Team> = teamMapOf()
+    }
+
+    // Generate teams for one round
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        val teams = teamMapOf<Team>()
+
+        // Create the teams, generate IDs, and store them
+        for (i in 0 until teamsNumber) {
+            val teamId = identifierFactory.uniqueIdentifier()
+            val team = Team(teamId) teamsStorage[teamId] = team  // Store the team in the teamsStorage
+            teams.add(team
