57b205b3f0de1ec1ba61ed1755e1499ca5198d91
iZIer01 <joaonkunku01@gmail.com>
2025-03-19 09:27:11 +0200

    fix to the result section

---
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResult.kt
@@ -0,0 +1,6 @@
+package jetbrains.kotlin.course.alias.results
+
+import jetbrains.kotlin.course.alias.team.Team
+
+// GameResult represents the outcome of a game as a list of teams
+typealias GameResult = List<Team>
--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
@@ -1,23 +1,46 @@
 package jetbrains.kotlin.course.alias.results
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import jetbrains.kotlin.course.alias.team.Team
 import jetbrains.kotlin.course.alias.team.TeamService
+import org.springframework.stereotype.Service
+import java.io.File
 
-// Type alias to store game results
-typealias GameResult = List<Team>
-
+@Service
 class GameResultsService {
 
     companion object {
-        private val gameHistory: MutableList<GameResult> = mutableListOf()
+        val gameHistory: MutableList<GameResult> = mutableListOf()
+        private val objectMapper = jacksonObjectMapper()
+        private const val GAME_HISTORY_FILE = "game_history.json"
+
+        // Load game history when the service starts
+        init {
+            loadGameHistory()
+        }
+
+        fun saveGameHistory() {
+            objectMapper.writeValue(File(GAME_HISTORY_FILE), gameHistory)
+        }
+
+        private fun loadGameHistory() {
+            val file = File(GAME_HISTORY_FILE)
+            if (file.exists()) {
+                gameHistory.addAll(objectMapper.readValue(file))
+            }
+        }
     }
 
     fun saveGameResults(result: GameResult) {
-        require(result.isNotEmpty()) { "Result cannot be empty" }
-        require(result.all { TeamService.teamsStorage.containsKey(it.id) }) {
-            "All team IDs must be valid"
+        require(result.isNotEmpty()) { "Game result cannot be empty." }
+        require(result.all { it.id in TeamService.teamsStorage }) {
+            "Invalid team found in the result. All teams must exist in TeamService."
         }
         gameHistory.add(result)
+
+        // Persist game history after each save
+        saveGameHistory()
     }
 
     fun getAllGameResults(): List<GameResult> = gameHistory.reversed()
